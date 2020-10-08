48d399e81ac8e1258a00e02f2597b51c5af0254a
gabbs <gortman@rednovalabs.com>
2020-10-08 10:30:38 -0500

    update about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,21 +4,25 @@ import React from 'react';
 const AboutMe = () => (
   <React.Fragment>
     <p>
-      Hi, I&apos;m <span>Gabby Ortman</span>. I&apos;m currently a software engineer at <a href="https://www.storable.com/">Storable</a> (formerly <span>Red Nova Labs</span>) primarily working in a JavaScript-centered stack and contribute to both the frontend and backend. When I&apos;m not working with JavaScript, I may be contributing to other repositories with Ruby on Rails or Angular or other hip 2010s web development technologies.
+    Hi, I&apos;m <span>Gabby Ortman</span>. I&apos;m currently a software engineer at <a href="https://www.storable.com/" target="_blank" rel="noreferrer noopener">Storable</a> (formerly <span>Red Nova Labs</span>) primarily working with JavaScript and contribute to both the frontend and backend on a full-remote team. Notably, I work on the team that launched the revamp of StorageMart's new <a href="https://www.storage-mart.com/" target="_blank" rel="noreferrer noopener">website</a> and website builder. When I&apos;m not working with JavaScript, I may be contributing to other repositories built with Ruby on Rails or Angular or other hip 2010s web development technologies.
     </p>
     <hr />
     <p>
-      During normal circumstances, you might find me attending conferences, grimacing at my computer in local coffee shops, running, and spoiling my dog, <span>Pius</span>, and rabbit, <span>Smalls</span>.
+    During normal circumstances, you might find me attending conferences, grimacing at my computer in local coffee shops, running, and spoiling my dog, <a href="https://www.instagram.com/antoninus.pius.dog/" target="_blank" rel="noreferrer noopener">Pius</a>, and rabbit, <span>Smalls</span>.
     </p>
     <p>
-      Currently, you&apos;ll likely find me running, ordering an iced mocha to-go from either Monarch or Broadway Coffee, terraforming my animal crossing island, camping, and walking my dog around the neighborhood.
+      Currently, you&apos;ll probably find me running, ordering an iced mocha to-go from either Monarch or Broadway Coffee, terraforming my animal crossing island, camping, and walking Pius around the neighborhood.
     </p>
     <hr />
     <p>
-      I studied computer science at <span>Iowa State University</span> and graduated in 2017. While there, I kept myself busy running <a href="http://digitalwomen.ece.iastate.edu/">Digital Women</a>, acting as a student liason on the <span>Computer Science Student Advisory Council</span>, participating in <span>hackathons</span>, and volunteering at STEM events.
+      I studied computer science at <span>Iowa State University</span> and graduated in 2017. While there, I kept myself busy running <a href="http://digitalwomen.ece.iastate.edu/" target="_blank" rel="noreferrer noopener">Digital Women</a>, acting as a student liason on the <a href="https://www.cs.iastate.edu/student-advisory-council" target="_blank" rel="noreferrer noopener">Computer Science Student Advisory Council</a>, participating in <span>hackathons</span>, and volunteering at STEM events.
     </p>
     <p>
-      Before <span>Storable</span>, I did three software engineering internships in Kansas City and one NSF REU at Iowa State University.
+      Before <span>Storable</span>, I did three software engineering internships in Kansas City.
+    </p>
+    <hr />
+    <p>
+      If you want to chat about my dog, the bugs I've personally introduced at work, why I'm not using <em>[really cool webpack plugin]</em> on my website, or need a recommendation on where to get coffee in Midtown/Westport, Kansas City, feel free to contact me on <a href="https://keybase.io/gabbyortman" target="_blank" rel="noreferrer noopener">Keybase</a> or by <a href="mailto:hi@gabbyortman.com">email</a>.
     </p>
   </React.Fragment>
 );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,17 +5,17 @@ const Navigation = () => (
   <nav>
     <Nav>
       <Nav.Item>
-        <Nav.Link href="https://github.com/nucle0tides">
+        <Nav.Link href="https://github.com/nucle0tides" target="_blank" rel="noreferrer noopener">
           GitHub
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="https://gitlab.com/nucle0tides">
+        <Nav.Link href="https://gitlab.com/nucle0tides" target="_blank" rel="noreferrer noopener">
           GitLab
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="https://keybase.io/gabbyortman">
+        <Nav.Link href="https://keybase.io/gabbyortman" target="_blank" rel="noreferrer noopener">
           Keybase
         </Nav.Link>
       </Nav.Item>
